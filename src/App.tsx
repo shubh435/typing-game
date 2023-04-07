6b096682d8cbf74eb448c2ce02dfed8fb8c2a311
shubham sarode <shubham.sarode@extwebtech.in>
2023-04-07 22:49:41 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { randomWordsData } from "./data";
+import "./App.css";
+interface AppProps {}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppState {
+  time: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      time: 60,
+    };
+  }
+  timerInterval?: NodeJS.Timer;
+  countDownHandler = () => {
+    this.timerInterval = setInterval(() => {
+      if (this.state.time < 1) {
+        clearInterval(this.timerInterval);
+      } else {
+        this.setState({ time: this.state.time - 1 });
+      }
+    }, 1000);
+  };
+
+  render() {
+    return (
+      <>
+        <button className="button" onClick={() => this.countDownHandler()}>
+          start
+        </button>
+        <div className="time">
+          <h1>time</h1>
+          <p>{this.state.time}</p>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
